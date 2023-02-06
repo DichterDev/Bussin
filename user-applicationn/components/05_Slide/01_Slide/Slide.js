0fe84fce9f43a3f5683e2f8c4f2dc51ca5fb5ddb
RuppDarwin <ruppdarwin@gmail.com>
2023-02-06 09:25:08 +0100

    key = error

--- user-applicationn/components/05_Slide/01_Slide/Slide.js
+++ user-applicationn/components/05_Slide/01_Slide/Slide.js
@@ -7,58 +7,44 @@ constructor(props) {
     super(props);
     this.state = {
         categes: [
-        { cat_id: '1', cat_name: {this: props.PointOne}, backgroundcolor: '#ED2525' },
-        { cat_id: '2', cat_name: {this: props.PointTwo}, backgroundcolor: '#ED2525' },
-        { cat_id: '3', cat_name: {this: props.PointThree}, backgroundcolor: '#ED2525' },
-        { cat_id: '4', cat_name: {this: props.PointFour}, backgroundcolor: '#ED2525' },
+        { key: 1, cat_id: '1', cat_name: props.PointOne, backgroundcolor: '#EBEBEB' },
+        { key: 2, cat_id: '2', cat_name: props.PointTwo, backgroundcolor: '#CCCCCC' },
+        { key: 3, cat_id: '3', cat_name: props.PointThree, backgroundcolor: '#CCCCCC' },
+        { key: 4, cat_id: '4', cat_name: props.PointFour, backgroundcolor: '#CCCCCC' },
         ],
         change: false,
     };
     }
 
-    changeBackground = item => {
+    changeBackground = (item, key) => {
         let categes = JSON.parse(JSON.stringify(this.state.categes));
     
         for (let x = 0; x < this.state.categes.length; x++) {
           if (this.state.categes[x].cat_id == item.cat_id) {
-            categes[x].backgroundcolor = '#0000FF';
-    
+            categes[x].backgroundcolor = '#EBEBEB';
+            
             this.setState({
               categes: categes,
             });
           } else {
-            categes[x].backgroundcolor = '#ED2525';
+            categes[x].backgroundcolor = '#CCCCCC';
     
             this.setState({
               categes: categes,
             });
           }
+          key = this.state.categes[x].key;
         }
       };
   render() {
     return (
         <View className="flex-1 justify-center items-center">
-            <View className="flex-row bg-secondary-color rounded-lg">
-            {this.state.categes.map((item, key) => (
-                <TouchableOpacity style={{backgroundColor: item.backgroundcolor,}} onPress={() => this.changeBackground(item)} className="bg-font-color px-6 py-2 m-1 rounded-lg">
-                    <Text>{item.cat_name}</Text>
-                </TouchableOpacity>
-            ))}
-            {this.state.categes.map((item, key) => (
-                <TouchableOpacity style={{backgroundColor: item.backgroundcolor,}} onPress={() => this.changeBackground(item)} className="px-6 py-2 m-1 rounded-lg">
-                    <Text>{item.cat_name}</Text>
-                </TouchableOpacity>
-            ))}
-            {this.state.categes.map((item, key) => (
-                <TouchableOpacity style={{backgroundColor: item.backgroundcolor,}} onPress={() => this.changeBackground(item)} className="px-6 py-2 m-1 rounded-lg">
-                    <Text>{item.cat_name}</Text>
-                </TouchableOpacity>
-            ))}
-            {this.state.categes.map((item, key) => (
-                <TouchableOpacity style={{backgroundColor: item.backgroundcolor,}} onPress={() => this.changeBackground(item)} className="px-6 py-2 m-1 rounded-lg">
-                    <Text>{item.cat_name}</Text>
-                </TouchableOpacity>
-            ))}
+            <View className="flex-row bg-secondary-color rounded-lg w-3/4 justify-between">
+              {this.state.categes.map((item, key) => (
+                  <TouchableOpacity style={{backgroundColor: item.backgroundcolor,}} onPress={() => this.changeBackground(item, key)} className="bg-font-color py-2 px-1 m-1 rounded-lg">
+                      <Text>{item.cat_name}</Text>
+                  </TouchableOpacity>
+              ))}
             </View>
         </View>
     )
